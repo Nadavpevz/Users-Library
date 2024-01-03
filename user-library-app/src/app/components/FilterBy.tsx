36b9d32fef2692416a9b224b0dda9787b1d81303
Nadavpevz <nadavpevz@gmail.com>
2024-01-03 23:48:56 +0200

    Update FilterBy.tsx
    fixed responsive issue

--- user-library-app/src/app/components/FilterBy.tsx
+++ user-library-app/src/app/components/FilterBy.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Input } from "@chakra-ui/react";
+import { Input, useMediaQuery } from "@chakra-ui/react";
 import React from "react";
 import { FilterParameterOptions } from "../types";
 import { useDispatch } from "react-redux";
@@ -9,9 +9,13 @@ const FilterBy: React.FC<{
   filterParameter: FilterParameterOptions;
 }> = ({ filterParameter }) => {
   const dispatch = useDispatch();
+  const [isSmallerThan700] = useMediaQuery("(max-width: 700px)");
   return (
     <Input
-      placeholder={`Filter by ${filterParameter}`}
+      placeholder={
+        isSmallerThan700 ? filterParameter : `Filter by ${filterParameter}`
+      }
+      fontSize={isSmallerThan700 ? "small" : "medium"}
       border={"1px solid black"}
       onChange={(e) =>
         dispatch(setFilter({ filterParameter, input: e.target.value }))
